474431b19a2a7a73cbbca7bd5cd5d7bcb4cb9836
omaretskyeh <omaretskyeh@gmail.com>
2025-11-23 18:08:05 -0500

    improved UI

--- app/src/main/java/com/example/smartair/homepages/HomepageActivity.java
+++ app/src/main/java/com/example/smartair/homepages/HomepageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +17,8 @@ import com.example.smartair.pre_post_checks.PrePostCheckActivity;
 import com.example.smartair.technique_guidance.TechniqueActivity;
 import com.example.smartair.ui.child.ChildPEFActivity;
 import com.example.smartair.ui.child.ChildTriageActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class HomepageActivity extends AppCompatActivity {
@@ -26,12 +29,14 @@ public class HomepageActivity extends AppCompatActivity {
     private Button btnBadge; // 只声明，不初始化
     private Button btnCheckPeakFlow;
     private Button btnCheckSymptom;
+    private TextView textGreeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage); // 确保布局文件存在
-
+        setContentView(R.layout.activity_homepage);
+        // 确保布局文件存在
+        textGreeting = findViewById(R.id.textGreeting);
         // 初始化按钮
         btnLogMedicine = findViewById(R.id.btnLogMedicine);
         btnTechnique = findViewById(R.id.btnTechnique);
@@ -56,6 +61,32 @@ public class HomepageActivity extends AppCompatActivity {
                 startActivity(new Intent(this, ChildPEFActivity.class)));
         btnCheckSymptom.setOnClickListener(v ->
                 startActivity(new Intent(this, ChildTriageActivity.class)));
-
+        loadChildUsername();
     }
+    private void loadChildUsername() {
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        FirebaseFirestore.getInstance()
+                .collection("users")
+                .document(uid)
+                .get()
+                .addOnSuccessListener(doc -> {
+                    if (doc.exists()) {
+                        String username = doc.getString("username");
+
+                        if (username != null && !username.isEmpty()) {
+                            textGreeting.setText("Hello, " + username + "!");
+                        } else {
+                            textGreeting.setText("Hello, Child!");
+                        }
+                    } else {
+                        textGreeting.setText("Hello, Child!");
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    textGreeting.setText("Hello, Child!");
+                });
+    }
+
 }
+
